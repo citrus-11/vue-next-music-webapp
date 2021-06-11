6f835a5e4d3923da8552a992a60b57f4c14736e3
CITRUS <citrus11@foxmail.com>
2021-06-11 18:19:38 +0800

    集成滚动组件

--- src/views/Recommend.vue
+++ src/views/Recommend.vue
@@ -1,32 +1,36 @@
 <template>
   <div class="recommend">
-    <!-- 轮播图 -->
-    <div class="slider-wrapper">
-      <div class="slider-content">
-        <slider v-if="sliders.length" :sliders="sliders" />
+    <scroll class="recommend-content">
+      <div>
+        <!-- 轮播图 -->
+        <div class="slider-wrapper">
+          <div class="slider-content">
+            <slider v-if="sliders.length" :sliders="sliders" />
+          </div>
+        </div>
+
+        <!-- 热门推荐列表 -->
+        <div class="recommend-list">
+          <h1 class="list-title">热门歌单推荐</h1>
+          <ul>
+            <li v-for="item in albums" :key="item.id" class="item">
+              <div class="icon">
+                <img :src="item.pic" width="60" height="60" />
+              </div>
+
+              <div class="text">
+                <h2 class="name">
+                  {{ item.username }}
+                </h2>
+                <p class="title">
+                  {{ item.title }}
+                </p>
+              </div>
+            </li>
+          </ul>
+        </div>
       </div>
-    </div>
-
-    <!-- 热门推荐列表 -->
-    <div class="recommend-list">
-      <h1 class="list-title">热门歌单推荐</h1>
-      <ul>
-        <li v-for="item in albums" :key="item.id" class="item">
-          <div class="icon">
-            <img :src="item.pic" width="60" height="60" />
-          </div>
-
-          <div class="text">
-            <h2 class="name">
-              {{ item.username }}
-            </h2>
-            <p class="title">
-              {{ item.title }}
-            </p>
-          </div>
-        </li>
-      </ul>
-    </div>
+    </scroll>
   </div>
 </template>
 
@@ -37,10 +41,14 @@ import getRecommend from '@/service/recommend'
 // 轮播图组件
 import Slider from '@/components/base/slider/Slider'
 
+// 滚动组件
+import Scroll from '@/components/base/scroll/Scroll'
+
 export default {
   name: 'Recommend',
   components: {
     Slider,
+    Scroll,
   },
   data() {
     return {
@@ -65,60 +73,65 @@ export default {
   bottom: 0;
   overflow: scroll;
 
-  .slider-wrapper {
-    position: relative;
-    width: 100%;
-    height: 0;
-    padding-top: 40%;
+  .recommend-content {
+    height: 100%;
     overflow: hidden;
 
-    .slider-content {
-      position: absolute;
-      left: 0;
-      top: 0;
+    .slider-wrapper {
+      position: relative;
       width: 100%;
-      height: 100%;
-    }
-  }
+      height: 0;
+      padding-top: 40%;
+      overflow: hidden;
 
-  .recommend-list {
-    .list-title {
-      height: 65px;
-      line-height: 65px;
-      text-align: center;
-      font-size: $font-size-medium;
-      color: $color-theme;
-    }
-
-    .item {
-      display: flex;
-      box-sizing: border-box;
-      align-items: center;
-      padding: 0 20px 20px 20px;
-
-      .icon {
-        flex: 0 0 60px;
-        width: 60px;
-        padding-right: 20px;
+      .slider-content {
+        position: absolute;
+        left: 0;
+        top: 0;
+        width: 100%;
+        height: 100%;
       }
+    }
 
-      .text {
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        flex: 1;
-        line-height: 20px;
-        overflow: hidden;
+    .recommend-list {
+      .list-title {
+        height: 65px;
+        line-height: 65px;
+        text-align: center;
         font-size: $font-size-medium;
+        color: $color-theme;
       }
 
-      .name {
-        margin-bottom: 10px;
-        color: $color-text;
-      }
+      .item {
+        display: flex;
+        box-sizing: border-box;
+        align-items: center;
+        padding: 0 20px 20px 20px;
 
-      .title {
-        color: $color-text-d;
+        .icon {
+          flex: 0 0 60px;
+          width: 60px;
+          padding-right: 20px;
+        }
+
+        .text {
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          flex: 1;
+          line-height: 20px;
+          overflow: hidden;
+          font-size: $font-size-medium;
+        }
+
+        .name {
+          margin-bottom: 10px;
+          color: $color-text;
+        }
+
+        .title {
+          color: $color-text-d;
+        }
       }
     }
   }

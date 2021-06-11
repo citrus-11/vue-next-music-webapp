b1801d163c2324dec801fc3fad2cc852875a402e
CITRUS <citrus11@foxmail.com>
2021-06-11 16:52:27 +0800

    集成轮播图组件

--- src/views/Recommend.vue
+++ src/views/Recommend.vue
@@ -1,15 +1,55 @@
 <template>
-  <div class="recommend">推荐</div>
+  <div class="recommend">
+    <div class="slider-wrapper">
+      <div class="slider-content">
+        <slider v-if="sliders.length" :sliders="sliders" />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import { getRecommend } from '@/service/recommend'
+import getRecommend from '@/service/recommend'
+import Slider from '@/components/base/slider/Slider'
 
 export default {
   name: 'Recommend',
+  components: {
+    Slider,
+  },
+  data() {
+    return {
+      sliders: [],
+    }
+  },
   async created() {
     const result = await getRecommend()
-    console.log(result)
+    this.sliders = result.sliders
   },
 }
 </script>
+
+<style lang="scss" scoped>
+.recommend {
+  position: fixed;
+  width: 100%;
+  top: 88px;
+  bottom: 0;
+
+  .slider-wrapper {
+    position: relative;
+    width: 100%;
+    height: 0;
+    padding-top: 40%;
+    overflow: hidden;
+
+    .slider-content {
+      position: absolute;
+      left: 0;
+      top: 0;
+      width: 100%;
+      height: 100%;
+    }
+  }
+}
+</style>

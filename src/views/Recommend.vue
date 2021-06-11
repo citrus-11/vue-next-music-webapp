58db26ac9d836f54d61b77b5f99eb1e70126e7b2
CITRUS <citrus11@foxmail.com>
2021-06-11 17:12:14 +0800

    完成热门推荐列表

--- src/views/Recommend.vue
+++ src/views/Recommend.vue
@@ -1,15 +1,40 @@
 <template>
   <div class="recommend">
+    <!-- 轮播图 -->
     <div class="slider-wrapper">
       <div class="slider-content">
         <slider v-if="sliders.length" :sliders="sliders" />
       </div>
     </div>
+
+    <!-- 热门推荐列表 -->
+    <div class="recommend-list">
+      <h1 class="list-title">热门歌单推荐</h1>
+      <ul>
+        <li v-for="item in albums" :key="item.id" class="item">
+          <div class="icon">
+            <img :src="item.pic" width="60" height="60" />
+          </div>
+
+          <div class="text">
+            <h2 class="name">
+              {{ item.username }}
+            </h2>
+            <p class="title">
+              {{ item.title }}
+            </p>
+          </div>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
+// 轮播图和热门推荐类别接口
 import getRecommend from '@/service/recommend'
+
+// 轮播图组件
 import Slider from '@/components/base/slider/Slider'
 
 export default {
@@ -20,11 +45,14 @@ export default {
   data() {
     return {
       sliders: [],
+      albums: [],
     }
   },
+  // 实例创建后异步获取接口数据
   async created() {
     const result = await getRecommend()
     this.sliders = result.sliders
+    this.albums = result.albums
   },
 }
 </script>
@@ -35,6 +63,7 @@ export default {
   width: 100%;
   top: 88px;
   bottom: 0;
+  overflow: scroll;
 
   .slider-wrapper {
     position: relative;
@@ -49,6 +78,48 @@ export default {
       top: 0;
       width: 100%;
       height: 100%;
+    }
+  }
+
+  .recommend-list {
+    .list-title {
+      height: 65px;
+      line-height: 65px;
+      text-align: center;
+      font-size: $font-size-medium;
+      color: $color-theme;
+    }
+
+    .item {
+      display: flex;
+      box-sizing: border-box;
+      align-items: center;
+      padding: 0 20px 20px 20px;
+
+      .icon {
+        flex: 0 0 60px;
+        width: 60px;
+        padding-right: 20px;
+      }
+
+      .text {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        flex: 1;
+        line-height: 20px;
+        overflow: hidden;
+        font-size: $font-size-medium;
+      }
+
+      .name {
+        margin-bottom: 10px;
+        color: $color-text;
+      }
+
+      .title {
+        color: $color-text-d;
+      }
     }
   }
 }

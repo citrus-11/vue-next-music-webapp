74a468064b8c5f0f9cb491e15e6d007740403027
CITRUS <citrus11@foxmail.com>
2021-06-14 20:44:36 +0800

    歌手详情和歌手数据接口

--- src/service/singer.js
+++ src/service/singer.js
@@ -1,6 +1,12 @@
 import { get } from './base'
 
 // 获取歌手接口
-export default function getSingerList() {
+export function getSingerList() {
   return get('/api/getSingerList')
+}
+
+export function getSingerDetail(singer) {
+  return get('/api/getSingerDetail', {
+    mid: singer.mid,
+  })
 }

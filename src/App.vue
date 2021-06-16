4a5c70b6c780cefb4700bab4db7a27121dbb7a86
CITRUS <citrus11@foxmail.com>
2021-06-16 10:30:46 +0800

    完成音乐基础播放和回退逻辑

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,20 @@
   <m-header />
   <tab />
   <router-view />
+  <!-- 播放器为全局组件 -->
+  <player />
 </template>
 
 <script>
 import MHeader from '@/components/header/MHeader'
 import Tab from '@/components/tab/Tab'
+import Player from './components/player/Player.vue'
 
 export default {
   components: {
     MHeader,
     Tab,
+    Player,
   },
 }
 </script>

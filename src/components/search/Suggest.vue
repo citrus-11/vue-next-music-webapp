80e5cee1e8344424ed3316b48a229cc40db7cf9e
CITRUS <citrus11@foxmail.com>
2021-06-23 12:29:07 +0800

    完成基本搜索列表组件功能

---
+++ src/components/search/Suggest.vue
@@ -0,0 +1,123 @@
+<template>
+  <div
+    ref="rootRef"
+    class="suggest"
+    v-loading:[loadingText]="loading"
+    v-no-result:[noResultText]="noResult"
+  >
+    <ul class="suggest-list">
+      <li class="suggest-item" v-if="singer">
+        <div class="icon">
+          <i class="icon-mine" />
+        </div>
+        <div class="name">
+          <p class="text">{{ singer.name }}</p>
+        </div>
+      </li>
+      <li class="suggest-item" v-for="song in songs" :key="song.id">
+        <div class="icon">
+          <i class="icon-music" />
+        </div>
+        <div class="name">
+          <p class="text">{{ song.singer }}-{{ song.name }}</p>
+        </div>
+      </li>
+      <div class="suggest-item" />
+    </ul>
+  </div>
+</template>
+
+<script>
+import { computed, ref, watch } from 'vue'
+import { search } from '@/service/search'
+import { processSongs } from '@/service/song'
+
+export default {
+  name: 'Suggest',
+  props: {
+    query: String,
+    showSinger: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  setup(props) {
+    const singer = ref(null)
+    const songs = ref([])
+    const hasMore = ref(false)
+    const page = ref(1)
+    const loadingText = ref('')
+    const noResultText = ref('抱歉，暂无搜索结果')
+
+    const loading = computed(() => {
+      return !singer.value && !songs.value.length
+    })
+
+    const noResult = computed(() => {
+      return !singer.value && !songs.value.length && !hasMore.value
+    })
+
+    watch(
+      // 通过 getter 获取最新 query
+      () => props.query,
+      async newQuery => {
+        if (!newQuery) return
+        await searchFirst()
+      },
+    )
+
+    // 初始化并搜索
+    async function searchFirst() {
+      page.value = 1
+      songs.value = []
+      singer.value = null
+      hasMore.value = true
+
+      const result = await search(props.query, page.value, props.showSinger)
+      songs.value = await processSongs(result.songs)
+      singer.value = result.singer
+      hasMore.value = result.hasMore
+    }
+
+    return { singer, songs, loadingText, loading, noResult, noResultText }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.suggest {
+  height: 100%;
+  overflow: hidden;
+
+  .suggest-list {
+    padding: 0 30px;
+
+    .suggest-item {
+      display: flex;
+      align-items: center;
+      padding-bottom: 20px;
+
+      .icon {
+        flex: 0 0 30px;
+        width: 30px;
+
+        [class^='icon-'] {
+          font-size: 14px;
+          color: $color-text-d;
+        }
+      }
+
+      .name {
+        flex: 1;
+        font-size: $font-size-medium;
+        color: $color-text-d;
+        overflow: hidden;
+
+        .text {
+          @include no-wrap();
+        }
+      }
+    }
+  }
+}
+</style>

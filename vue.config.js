be357579e885265e13e0af007dd549d0ec35726f
CITRUS <citrus11@foxmail.com>
2021-06-25 17:47:13 +0800

    添加分析依赖配置

--- vue.config.js
+++ vue.config.js
@@ -18,4 +18,12 @@ module.exports = {
       registerRouter(app)
     },
   },
+  // 分析依赖 npm run build/serve --report
+  configureWebpack: config => {
+    if (process.env.npm_config_report) {
+      const BundleAnalyzerPlugin =
+        require('webpack-bundle-analyzer').BundleAnalyzerPlugin
+      config.plugins.push(new BundleAnalyzerPlugin())
+    }
+  },
 }
